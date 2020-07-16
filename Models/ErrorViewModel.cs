3555d1ad957ddd29d3629c252e69a415f2432106
shubham <shubhamdeulkar27@gmail.com>
2020-07-16 08:53:14 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureDemoOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
